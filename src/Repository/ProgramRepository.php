8546322b77e512a45605dea2d5b6a2a46596b87a
olivier-preaux <olivier.prx@gmail.com>
2021-01-31 14:25:09 +0100

    Quete 22

--- src/Repository/ProgramRepository.php
+++ src/Repository/ProgramRepository.php
@@ -20,42 +20,16 @@ class ProgramRepository extends ServiceEntityRepository
     }
 
     public function findLikeName(string $name)
-    {
+    {   
         $queryBuilder = $this->createQueryBuilder('p')
-            ->where('p.title LIKE :name')
-            ->setParameter('name', '%' . $name . '%')
-            ->orderBy('p.title', 'ASC')
-            ->getQuery();
+        ->leftjoin( 'p.actors' ,'a')
+        ->where('p.title LIKE :name')
+        ->orWhere('a.name LIKE :name')
+        ->setParameter('name', '%' . $name . '%')
+        ->orderBy('p.title', 'ASC')
+        ->getQuery();
 
         return $queryBuilder->getResult();
     }
 
-    // /**
-    //  * @return Program[] Returns an array of Program objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Program
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
